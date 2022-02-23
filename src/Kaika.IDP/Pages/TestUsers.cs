7e6aaf8c73eb27b22e3e162520b73b4a8b2e7666
Florian Pirard <florian.pirard@realdolmen.com>
2022-02-23 22:08:58 +0100

    add UI + testusers

---
+++ src/Kaika.IDP/Pages/TestUsers.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Test;
+
+namespace Kaika.IDP;
+
+public class TestUsers
+{
+    public static List<TestUser> Users = new List<TestUser>
+    {
+        new TestUser{
+            SubjectId = "b6f7a7b1-f403-4434-8dac-8495ddd1f36e",
+            Username = "Frank",
+            Password = "password",
+            Claims = new List<Claim>{
+                new Claim("given_name", "Frank"),
+                new Claim("family_name", "Underwood")
+            }
+        },
+        new TestUser{
+            SubjectId = "b99748a7-ee9c-4e05-8c71-784dda2d2e87",
+            Username = "Claire",
+            Password = "password",
+            Claims = new List<Claim>{
+                new Claim("given_name", "Claire"),
+                new Claim("family_name", "Underwood")
+            }
+        },
+    };
+}
