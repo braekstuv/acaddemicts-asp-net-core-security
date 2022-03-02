b207ee9e5cd1a9487a79216cd48ef1b975422ee2
Florian Pirard <Florian.Pirard@realdolmen.com>
2022-03-02 17:13:21 +0100

    pass bearertoken to api

--- src/ImageGallery.Client/HostingExtensions.cs
+++ src/ImageGallery.Client/HostingExtensions.cs
@@ -1,5 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using IdentityModel;
+using ImageGallery.Client.HttpHandlers;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
@@ -16,13 +17,18 @@ public static class HostingExtensions
         builder.Services.AddControllersWithViews()
              .AddJsonOptions(opts => opts.JsonSerializerOptions.PropertyNamingPolicy = null);
 
+        builder.Services.AddHttpContextAccessor();
+
+        builder.Services.AddTransient<BearerTokenHandler>();
+
         // create an HttpClient used for accessing the API
         builder.Services.AddHttpClient("APIClient", client =>
         {
             client.BaseAddress = new Uri("https://localhost:44366/");
             client.DefaultRequestHeaders.Clear();
             client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
-        });
+        }).AddHttpMessageHandler<BearerTokenHandler>();
+
         // create an HttpClient used for accessing the IDP
         builder.Services.AddHttpClient("IDPClient", client =>
         {
@@ -49,7 +55,7 @@ public static class HostingExtensions
             options.ResponseType = "code";
             options.Scope.Add("address");
             options.Scope.Add("roles");
-            options.Scope.Add("imagegalleryapi");
+            options.Scope.Add("imagegalleryapi.read");
             options.ClaimActions.DeleteClaim("sid");
             options.ClaimActions.DeleteClaim("idp");
             options.ClaimActions.DeleteClaim("auth_time");
